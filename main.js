8af7158c4a45fc314fd481b2fbc92fefa16f78c4
asing124 <106757227+asing124@users.noreply.github.com>
2022-06-24 23:32:51 -0400

    Site build update 5

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
-$(document).ready(function () {
-	
+ $(document).ready(function () {
+ 	
 	var $hamburger = $(".hamburger");
 	$hamburger.on("click", function(e) {
 	$hamburger.toggleClass("is-active");
 	$('#nav').toggleClass('active');
 	
 });
+
 });
