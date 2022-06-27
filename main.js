6dad02063d4b8e95c40be33cabdc22d3040257db
asing124 <106757227+asing124@users.noreply.github.com>
2022-06-27 01:18:47 -0400

    Updates to image grid

--- main.js
+++ main.js
@@ -9,14 +9,14 @@
 
 });
 
- var i = 0;
-var txt = 'Style is a way to say who you are without having to speak'; /* The text */
-var speed = 50; /* The speed/duration of the effect in milliseconds */
+ /* var i = 0;
+var txt = 'Style is a way to say who you are without having to speak'; The text 
+var speed = 50;  The speed/duration of the effect in milliseconds 
 
 function typeWriter() {
   if (i < txt.length) {
     document.getElementById("demo").innerHTML += txt.charAt(i);
     i++;
-    setTimeout(typeWriter, speed);
+    setTimeout(typeWriter, speed); 
   }
-}
+}*/
